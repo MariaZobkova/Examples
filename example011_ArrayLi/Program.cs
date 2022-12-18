0d9d79747703da5cc8a496a3d012a550b21498d1
Maria <mzobkova@bk.ru>
2022-12-18 14:32:11 +0300

    example011_ArrayLi project

---
+++ example011_ArrayLi/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int [] collection)
+{
+    int size = collection.Length;
+    int index = 0;
+    
+    while (index < size)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int Len = col.Length;
+    int count = 0;
+
+    while (count < Len)
+    {
+        Console.WriteLine (col[count]);
+        count++;
+    } 
+}
+
+int IndexOf (int[] collection, int find)
+{
+    int size = collection.Length;
+    int index = 0;
+    int position = 0;
+    return position;
+
+    while (index < size)
+    {
+        if (collection [index]== find)
+        {
+            position = index;
+        }
+        index++;
+
+    }
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
